09e5fb8633c4c1b994886e1ba1271f30f5657e95
Sam Yaple <qrab@yaple.net>
2025-01-25 03:36:31 +0000

    Partial phase1 build
    need to move libffi to stage0

--- build.sh
+++ build.sh
@@ -3,17 +3,16 @@
 set -euxEo pipefail
 
 build() {
-    local phase=$1
-    shift
+    local phase=$1; shift
 
     podman build \
         --disable-compression \
         --volume ${PWD}/sources:/sources:O \
         --volume ${PWD}/git_sources:/git_sources:O \
-        --tag phase0 \
+        --tag ${phase} \
         --file Dockerfile-${phase} \
         phases
 }
 
-build phase0
-#build phase1
+#build phase0
+build phase1
